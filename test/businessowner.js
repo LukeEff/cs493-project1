2d6fdcf09d38e5cfd7c85383db5a5b4361ca15e4
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2023-04-12 12:38:02 -0700

    set up testing framework

---
+++ test/businessowner.js
@@ -0,0 +1,12 @@
+describe('Business Owner', function() {
+  describe('List businesses owned by a user', function() {
+    it('should return a list of businesses owned by a user', function() {
+      // TODO
+    });
+  });
+  describe('Create a new business', function() {
+    it('should create a new business', function() {
+      // TODO
+    });
+  });
+});
