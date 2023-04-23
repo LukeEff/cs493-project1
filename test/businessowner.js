b003abb26d9c4c06b4cc38e2a848186267f23905
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2023-04-23 08:04:04 -0700

    test create review endpoint

--- test/businessowner.js
+++ test/businessowner.js
@@ -5,62 +5,119 @@ var app = require('../app/app.js');
 
 let createdBusiness = {};
 
-describe('Businesses', function() {
-  describe('Business Owner', function () {
-    it('should create a new business', function () {
-      const url = 'http://localhost:3000/businesses/create';
-      const businessName = 'Test Business';
-      const streetAddress = '123 Test St';
-      const city = 'Test City';
-      const state = 'Test State';
-      const zipCode = '12345';
-      const businessPhoneNum = '123-456-7890';
-      const businessEmail = 'test@test.com';
-      const category = 'Test Category';
-      const website = 'www.test.com';
-      const ownerUuid = '12345';
-      const business = {
-        businessName: businessName,
-        streetAddress: streetAddress,
-        city: city,
-        state: state,
-        zipCode: zipCode,
-        businessPhoneNum: businessPhoneNum,
-        businessEmail: businessEmail,
-        category: category,
-        website: website,
-        ownerUuid: ownerUuid
-      };
-      request.post({
-        url: url,
-        body: JSON.stringify(business),
-        headers: {'Content-Type': 'application/json'}
-      }, function (error, response, body) {
-        expect(response.statusCode).to.equal(200);
-        expect(JSON.parse(body).businessName).to.equal(businessName);
-        createdBusiness = JSON.parse(body);
-      }, 10000);
-    });
-    it('should update a business', function () {
-      const url = 'http://localhost:3000/businesses/edit/' + createdBusiness.businessUuid;
-      const businessName = 'Test Business 2';
-      request.patch({
-        url: url,
-        body: JSON.stringify({businessName: businessName}),
-        headers: {'Content-Type': 'application/json'}
-      }, function (error, response, body) {
-        expect(response.statusCode).to.equal(200);
-        expect(JSON.parse(body).businessName).to.equal(businessName);
+describe('Endpoints', function() {
+  describe('Businesses', function () {
+    describe('Business Owner', function () {
+      it('should create a new business', function () {
+        const url = 'http://localhost:3000/businesses/create';
+        const businessName = 'Test Business';
+        const streetAddress = '123 Test St';
+        const city = 'Test City';
+        const state = 'Test State';
+        const zipCode = '12345';
+        const businessPhoneNum = '123-456-7890';
+        const businessEmail = 'test@test.com';
+        const category = 'Test Category';
+        const website = 'www.test.com';
+        const ownerUuid = '12345';
+        const business = {
+          businessName: businessName,
+          streetAddress: streetAddress,
+          city: city,
+          state: state,
+          zipCode: zipCode,
+          businessPhoneNum: businessPhoneNum,
+          businessEmail: businessEmail,
+          category: category,
+          website: website,
+          ownerUuid: ownerUuid
+        };
+        request.post({
+          url: url,
+          body: JSON.stringify(business),
+          headers: {'Content-Type': 'application/json'}
+        }, function (error, response, body) {
+          expect(response.statusCode).to.equal(200);
+          expect(JSON.parse(body).businessName).to.equal(businessName);
+          createdBusiness = JSON.parse(body);
+        }, 10000);
+      });
+      it('should update a business', function () {
+        const url = 'http://localhost:3000/businesses/edit/' + createdBusiness.businessUuid;
+        const businessName = 'Test Business 2';
+        request.patch({
+          url: url,
+          body: JSON.stringify({businessName: businessName}),
+          headers: {'Content-Type': 'application/json'}
+        }, function (error, response, body) {
+          expect(response.statusCode).to.equal(200);
+          expect(JSON.parse(body).businessName).to.equal(businessName);
+        });
+      });
+      it('should delete a business', function () {
+        const url = 'http://localhost:3000/businesses/delete/' + createdBusiness.businessUuid;
+        request.delete(url, function (error, response, body) {
+          expect(response.statusCode).to.equal(200);
+        }, 10000);
       });
     });
-    it('should delete a business', function () {
-      const url = 'http://localhost:3000/businesses/delete/' + createdBusiness.businessUuid;
-      request.delete(url, function (error, response, body) {
-        expect(response.statusCode).to.equal(200);
-      }, 10000);
+    describe('Business User', function () {
+      before(function () {
+        const url = 'http://localhost:3000/businesses/create';
+        const businessName = 'Test Business';
+        const streetAddress = '123 Test St';
+        const city = 'Test City';
+        const state = 'Test State';
+        const zipCode = '12345';
+        const businessPhoneNum = '123-456-7890';
+        const businessEmail = 'test@test.com';
+        const category = 'Test Category';
+        const website = 'www.test.com';
+        const ownerUuid = '12345';
+        const business = {
+          businessName: businessName,
+          streetAddress: streetAddress,
+          city: city,
+          state: state,
+          zipCode: zipCode,
+          businessPhoneNum: businessPhoneNum,
+          businessEmail: businessEmail,
+          category: category,
+          website: website,
+          ownerUuid: ownerUuid
+        };
+        request.post({
+          url: url,
+          body: JSON.stringify(business),
+          headers: {'Content-Type': 'application/json'}
+        }, function (error, response, body) {
+          expect(response.statusCode).to.equal(200);
+          createdBusiness = JSON.parse(body);
+        }, 10000);
+      });
+      after(function () {
+        const url = 'http://localhost:3000/businesses/delete/' + createdBusiness.businessUuid;
+        request.delete(url, function (error, response, body) {
+          expect(response.statusCode).to.equal(200);
+        }, 10000);
+      });
+      it('should list all businesses', function () {
+        const url = 'http://localhost:3000/businesses';
+        request.get(url, function (error, response, body) {
+          expect(response.statusCode).to.equal(200);
+          expect(JSON.parse(body)).to.be.an('array');
+        }, 10000);
+      });
+      it('should list a business', function () {
+        const url = 'http://localhost:3000/businesses/' + createdBusiness.businessUuid;
+        request.get(url, function (error, response, body) {
+          expect(response.statusCode).to.equal(200);
+          expect(JSON.parse(body)).to.be.an('object');
+        }, 10000);
+      });
     });
   });
-  describe('Business User', function () {
+  describe('Reviews', function () {
     before(function () {
       const url = 'http://localhost:3000/businesses/create';
       const businessName = 'Test Business';
@@ -94,26 +151,30 @@ describe('Businesses', function() {
         createdBusiness = JSON.parse(body);
       }, 10000);
     });
-    after(function () {
-      const url = 'http://localhost:3000/businesses/delete/' + createdBusiness.businessUuid;
-      request.delete(url, function (error, response, body) {
-        expect(response.statusCode).to.equal(200);
-      }, 10000);
-    });
-    it('should list all businesses', function () {
-      const url = 'http://localhost:3000/businesses';
-      request.get(url, function (error, response, body) {
-        expect(response.statusCode).to.equal(200);
-        expect(JSON.parse(body)).to.be.an('array');
-      }, 10000);
-    });
-    it('should list a business', function () {
-      const url = 'http://localhost:3000/businesses/' + createdBusiness.businessUuid;
-      request.get(url, function (error, response, body) {
-        expect(response.statusCode).to.equal(200);
-        expect(JSON.parse(body)).to.be.an('object');
-      }, 10000);
+    describe('Users', function () {
+      it('should create a new review', function () {
+        const url = 'http://localhost:3000/reviews/create';
+        const businessUuid = createdBusiness.businessUuid;
+        const userUuid = '12345';
+        const starRating = 5;
+        const moneyRating = 5;
+        const writtenReview = 'Test Review';
+        const reviewObj = {
+          businessUuid: businessUuid,
+          userUuid: userUuid,
+          starRating: starRating,
+          moneyRating: moneyRating,
+          writtenReview: writtenReview
+        };
+        request.post({
+          url: url,
+          body: JSON.stringify(reviewObj),
+          headers: {'Content-Type': 'application/json'}
+        }, function (error, response, body) {
+          expect(response.statusCode).to.equal(200);
+          expect(JSON.parse(body).businessUuid).to.equal(businessUuid);
+        }, 10000);
+      });
     });
   });
 });
-
